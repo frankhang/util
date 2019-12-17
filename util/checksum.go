b72c84fd266b9f4abb6446676ec5c69819802bf9
yuanhang <yuanhang@jd.com>
2019-12-18 00:36:45 +0800

    yh

--- util/checksum.go
+++ util/checksum.go
@@ -1,10 +1,17 @@
 package util
 
+func SignedSum(data []byte) int {
+	var s int
+	for _, b := range data {
+		s += int(b)
+	}
+	return s
+}
 
 func Sum(data []byte) int {
 	var s int
-	for b := range  data {
-		s += int(b)
+	for _, b := range data {
+		s += int(uint8(b))
 	}
 	return s
 }
